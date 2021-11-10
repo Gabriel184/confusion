ff3e00ae86886d0a686f64b627e6fb2dffae65a3
Gabriel Oliveira <gabriel.oliveira184@fatec.sp.gov.br>
2021-11-09 21:42:14 -0300

    Components - Part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from "reactstrap";
+import Menu from "./components/MenuComponent";
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Restoirante Con Fusion</NavbarBrand>
+    render() {
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Restoirante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu/>
             </div>
-        </Navbar>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
